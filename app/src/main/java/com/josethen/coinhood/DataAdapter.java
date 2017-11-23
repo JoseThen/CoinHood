7f35d766cefd94670e2424a6d2cd3ac698c93f4a
rappinkraken <thenj@ymail.com>
2017-11-23 15:39:54 -0500

    Get Skeleton Working

---
+++ app/src/main/java/com/josethen/coinhood/DataAdapter.java
@@ -0,0 +1,40 @@
+package com.josethen.coinhood;
+
+import com.robinhood.spark.SparkAdapter;
+
+import java.util.Random;
+
+
+
+public class DataAdapter extends SparkAdapter {
+    private final float[] yData;
+    private final Random random;
+
+    DataAdapter() {
+        random = new Random();
+        yData = new float[50];
+        randomize();
+    }
+
+    void randomize() {
+        for (int i = 0, count = yData.length; i < count; i++) {
+            yData[i] = random.nextFloat();
+        }
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return yData.length;
+    }
+
+    @Override
+    public Object getItem(int index) {
+        return yData[index];
+    }
+
+    @Override
+    public float getY(int index) {
+        return yData[index];
+    }
+}
--- app/src/main/java/com/josethen/coinhood/MainActivity.java
+++ app/src/main/java/com/josethen/coinhood/MainActivity.java
@@ -2,12 +2,60 @@ package com.josethen.coinhood;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.robinhood.spark.SparkAdapter;
+import com.robinhood.spark.SparkView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private DataAdapter adapter;
+    private TextView scrubInfoTextView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SparkView sparkView =  findViewById(R.id.sparkview);
+        scrubInfoTextView = findViewById(R.id.numberTextView);
+
+        adapter = new DataAdapter();
+        sparkView.setAdapter(adapter);
+        sparkView.setScrubListener(new SparkView.OnScrubListener() {
+            @Override
+            public void onScrubbed(Object value) {
+                if (value == null) {
+                    scrubInfoTextView.setText(R.string.scrub_empty);
+                } else {
+                    scrubInfoTextView.setText(getString(R.string.scrub_format, value));
+                }
+            }
+        });
+
+        View button = findViewById(R.id.random_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adapter.randomize();
+            }
+        });
+
+//        scrubInfoTextView = (TextView) findViewById(R.id.scrub_info_textview);
+
     }
+
+
+
+
+
+
+
+
+
 }
+
